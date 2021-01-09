c7c327d3926f77e152051411e6cac531ac7bc918
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2021-01-09 16:44:46 -0300

    add-menu-lateral-topo-pesquisa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import {Container} from './style';
+import React, { useState } from 'react';
+import {Container, Search, Logos} from './style';
+import logo from '../../assets/logo.svg';
+import TextField, {Input} from '@material/react-text-field';
 
-const Home = () => <Container>Helooooo</Container>
 
+const Home = () => {
+    const [inputValue,setInputValue] = useState('');
+    return(
+    <Container>
+        <Search>
+            <Logos src={logo} alt="logo buscador restaurante"/>
+            <TextField
+                label='Pesquisar Restaurante'
+                outlined
+                ><Input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)} />
+            </TextField>
+        </Search>
+    </Container>
+    );
+};
 
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  //  background-color:${(props) =>props.theme.colors.primary };
+
+export const Container = styled.aside`
+  background-color:${(props) => props.theme.colors.background };
+  width: 360px;
+  height: 100vh;
+  overflow-y: auto;
 `;
 
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;    
+    justify-content: center;
+    background-color:#fff;
+    padding: 16px;
+`;
 
- 
+export const Logos = styled.img`
+  margin-bottom: 16px;
+`
